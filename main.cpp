e03f35d3a9354b26c822cc16fe38f02c4362322d
ShkulevMaksim <79005304+ShkulevMaksim@users.noreply.github.com>
2021-12-29 02:52:46 +0700

    т ттт

--- main.cpp
+++ main.cpp
@@ -6,22 +6,7 @@ const int cells = 20;
 const int celln = 20;
 HWND hwnd = GetConsoleWindow();
 HDC dc = GetDC(hwnd);
-void DrawPoint(double cx, double cy) {
-    HBRUSH brush = CreateSolidBrush(RGB(255, 50, 100));
-    HPEN pen1 = CreatePen(PS_NULL, 1, RGB(0, 0, 0));
-
-    SelectObject(dc, brush);
-    SelectObject(dc, pen1);
-    Ellipse(dc, (cx - 1) * cells + cx + (celln/2)*cells + (celln / 2) - (cells/2), shift + (celln - cy) * cells + celln - cy + 1 - (celln / 2) * cells - (celln / 2) - (cells / 2),
-
-            cx * cells + cx + (celln / 2) * cells + (celln / 2) - (cells / 2), shift + (celln - cy + 1) * cells + celln - cy + 1 - (celln / 2) * cells - (celln / 2) - (cells / 2));//задание коорданат квадрата, в котором нужно нарисовать круг
-
-    DeleteObject(brush);
-    DeleteObject(pen1);
-
-}
-
-void DrawNet() {
+void draw_grid() {
 
     int x = 0, y = 0;
 
@@ -29,7 +14,7 @@ void DrawNet() {
 
     SelectObject(dc, pen);
     while (x <= celln) {
-        MoveToEx(dc, x * cells + x, shift, 0);
+        MoveToEx(dc, x * cells + x, shift, nullptr);
         LineTo(dc, x * cells + x, shift + cells * celln + celln);
         x++;
     }
@@ -37,17 +22,17 @@ void DrawNet() {
     DeleteObject(pen);
     SelectObject(dc, pen);
     while (y <= celln) {
-        MoveToEx(dc, 0, shift + y * cells + y, 0);//перенос текущей позиции
-        LineTo(dc, cells * celln + celln, shift + y * cells + y);//провести линиюв
+        MoveToEx(dc, 0, shift + y * cells + y, nullptr);
+        LineTo(dc, cells * celln + celln, shift + y * cells + y);
         y++;
 
     }
 
-    DeleteObject(pen);//освобождение памяти, после использования пера
+    DeleteObject(pen);
 
 }
 
-void Ymn(double A[][4], double B[][4])
+void multiplication(double A[][4], double B[][4])
 {
     double C[4][4];
     for (int i = 0; i < 4; i++)
@@ -68,28 +53,28 @@ void Ymn(double A[][4], double B[][4])
 
 }
 
-void Polygon(double P[][4])
+void draw_poly(double P[][4])
 {
-    HPEN pen = CreatePen(PS_SOLID, 1, RGB(255, 255, 255));
+    HPEN pen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
 
     SelectObject(dc, pen);//выбор пера для контура круга
 
     for (int i = 0; i < 3; i++)
     {
-        MoveToEx(dc, (P[i][0] - 1) * cells + P[i][0] + (celln / 2) * cells + (celln / 2), shift + (celln - P[i][1]) * cells + celln - P[i][1] + 1 - (celln / 2) * cells - (celln / 2), 0);
+        MoveToEx(dc, (P[i][0] - 1) * cells + P[i][0] + (celln / 2) * cells + (celln / 2), shift + (celln - P[i][1]) * cells + celln - P[i][1] + 1 - (celln / 2) * cells - (celln / 2), nullptr);
         LineTo(dc, (P[i+1][0] - 1) * cells + P[i+1][0] + (celln / 2) * cells + (celln / 2), shift + (celln - P[i+1][1]) * cells + celln - P[i+1][1] + 1 - (celln / 2) * cells - (celln / 2));
 
     }
 
-    DeleteObject(pen);//освобождение памяти, псле использования пера
+    DeleteObject(pen);
 }
 
-void VecYmn(double A[][4], double B[][4])
+void vec_mult(double A[][4], double B[][4])
 {
     double C[4][4];
 
     for (int i = 0; i < 4; i++)
-        A[i][3] = 1;
+     A[i][3] = 1;
 
     for (int i = 0; i < 4; i++)
     {
@@ -118,65 +103,23 @@ void Clear()
 }
 int main()
 {
-    int Vert[3][8];//координаты вершин
-    int Pol[4][6];//массив полигонов
-    double Pol1[4][4] = { {1.000000, -1.000000, -1.000000, 0},
-                          {1.000000, -1.000000, 1.000000, 0},
-                          {-1.000000, -1.000000, 1.000000, 0},
-                          {-1.000000, -1.000000, -1.000000, 1} };
-    double Pol2[4][4] = { {1.000000, 1.000000, - 0.999999, 0},
-                          {-1.000000, 1.000000, - 1.000000, 0},
-                          {-1.000000, 1.000000, 1.000000, 0},
-                          {0.999999, 1.000000, 1.000001, 1} };
-    double Pol3[4][4] = { {1.000000, - 1.000000, - 1.000000, 0},
-                          {1.000000, 1.000000, - 0.999999, 0},
-                          {0.999999, 1.000000, 1.000001, 0},
-                          {1.000000, -1.000000, 1.000000, 1} };
-    double Pol4[4][4] = { {1.000000, -1.000000, 1.000000, 0},
-                          {0.999999, 1.000000, 1.000001, 0},
-                          {-1.000000, 1.000000, 1.000000, 0},
-                          {-1.000000, - 1.000000, 1.000000, 1} };
-    double Pol5[4][4] = { {-1.000000, - 1.000000, 1.000000, 0},
-                          {-1.000000, 1.000000, 1.000000, 0},
-                          {-1.000000, 1.000000, - 1.000000, 0},
-                          {-1.000000, - 1.000000, - 1.000000, 1} };
-    double Pol6[4][4] = { {1.000000, 1.000000, - 0.999999, 0},
-                          {1.000000, - 1.000000, - 1.000000, 0},
-                          {-1.000000, - 1.000000, - 1.000000, 0},
-                          {-1.000000, 1.000000, - 1.000000, 1} };
-    double dx=2, dy=0, dz=0;
-    double sx = 2, sy = 2, sz = 2;
-    double fi = 45 * M_PI / 180;
+    double dx=1, dy=2, dz=3;
+    double size_x = 2, size_y = 2, size_z = 2;
     double T[4][4]={ {1, 0, 0, 0},
                      {0, 1, 0, 0},
                      {0, 0, 1, 0},
                      {dx, dy, dz, 1} };
-    double S[4][4]= { {sx, 0, 0, 0},
-                      {0, sy, 0, 0},
-                      {0, 0, sz, 0},
-                      {0, 0, 0, 1} };
-    double Rx[4][4]= { {1, 0, 0, 0},
-                       {0, cos(fi), sin(fi), 0},
-                       {0, -sin(fi), cos(fi), 0},
-                       {0, 0, 0, 1} };
-    double Ry[4][4] = { {cos(fi), 0, -sin(fi), 0},
-                        {0, 1, 0, 0},
-                        {sin(fi), 0, cos(fi), 0},
-                        {0, 0, 0, 1} };
-    double Rz[4][4] = { {cos(fi), sin(fi), 0, 0},
-                        {-sin(fi), cos(fi), 0, 0},
-                        {0, 0, 1, 0},
-                        {0, 0, 0, 1} };
+    double S[4][4]= { {size_x, 0,      0,      0},
+                      {0,      size_y, 0,      0},
+                      {0,      0,      size_z, 0},
+                      {0,      0,      0,      1} };
     double M[4][4] = { {1, 0, 0, 0},
                        {0, 1, 0, 0},
                        {0, 0, 1, 1},
                        {0, 0, 0, 1} };
-    double Pol7[4][4] = { {1.000000, -1.000000, -1.000000, 0},
-                          {1.000000, -1.000000, 1.000000, 0},
-                          {-1.000000, -1.000000, 1.000000, 0},
-                          {-1.000000, -1.000000, -1.000000, 1} };
     for (double fi=0 * M_PI /180; fi>-1; fi+=1* M_PI /180)
     {
+        //draw_grid();
         double Pol1[4][4] = { {1.000000, -1.000000, -1.000000, 0},
                               {1.000000, -1.000000, 1.000000, 0},
                               {-1.000000, -1.000000, 1.000000, 0},
@@ -213,56 +156,49 @@ int main()
                             {-sin(fi), cos(fi), 0, 0},
                             {0, 0, 1, 0},
                             {0, 0, 0, 1} };
-        Ymn(Pol1, M);
-        Ymn(Pol2, M);
-        Ymn(Pol3, M);
-        Ymn(Pol4, M);
-        Ymn(Pol5, M);
-        Ymn(Pol6, M);
-        VecYmn(Pol1, T);
-        VecYmn(Pol2, T);
-        VecYmn(Pol3, T);
-        VecYmn(Pol4, T);
-        VecYmn(Pol5, T);
-        VecYmn(Pol6, T);
-        Ymn(Pol1, S);
-        Ymn(Pol2, S);
-        Ymn(Pol3, S);
-        Ymn(Pol4, S);
-        Ymn(Pol5, S);
-        Ymn(Pol6, S);
-        Ymn(Pol1, Rx);
-        Ymn(Pol2, Rx);
-        Ymn(Pol3, Rx);
-        Ymn(Pol4, Rx);
-        Ymn(Pol5, Rx);
-        Ymn(Pol6, Rx);
-        Ymn(Pol1, Ry);
-        Ymn(Pol2, Ry);
-        Ymn(Pol3, Ry);
-        Ymn(Pol4, Ry);
-        Ymn(Pol5, Ry);
-        Ymn(Pol6, Ry);
-        Ymn(Pol1, Rz);
-        Ymn(Pol2, Rz);
-        Ymn(Pol3, Rz);
-        Ymn(Pol4, Rz);
-        Ymn(Pol5, Rz);
-        Ymn(Pol6, Rz);
-        for (int i = 0; i < 4; i++) {
-            DrawPoint(Pol1[i][0], Pol1[i][1]);
-            DrawPoint(Pol2[i][0], Pol2[i][1]);
-            DrawPoint(Pol3[i][0], Pol3[i][1]);
-            DrawPoint(Pol4[i][0], Pol4[i][1]);
-            DrawPoint(Pol5[i][0], Pol5[i][1]);
-            DrawPoint(Pol6[i][0], Pol6[i][1]);
-        }
-        Polygon(Pol1);
-        Polygon(Pol2);
-        Polygon(Pol3);
-        Polygon(Pol4);
-        Polygon(Pol5);
-        Polygon(Pol6);
+        multiplication(Pol1, M);
+        multiplication(Pol2, M);
+        multiplication(Pol3, M);
+        multiplication(Pol4, M);
+        multiplication(Pol5, M);
+        multiplication(Pol6, M);
+        vec_mult(Pol1, T);
+        vec_mult(Pol2, T);
+        vec_mult(Pol3, T);
+        vec_mult(Pol4, T);
+        vec_mult(Pol5, T);
+        vec_mult(Pol6, T);
+        multiplication(Pol1, S);
+        multiplication(Pol2, S);
+        multiplication(Pol3, S);
+        multiplication(Pol4, S);
+        multiplication(Pol5, S);
+        multiplication(Pol6, S);
+        multiplication(Pol1, Rx);
+        multiplication(Pol2, Rx);
+        multiplication(Pol3, Rx);
+        multiplication(Pol4, Rx);
+        multiplication(Pol5, Rx);
+        multiplication(Pol6, Rx);
+        multiplication(Pol1, Ry);
+        multiplication(Pol2, Ry);
+        multiplication(Pol3, Ry);
+        multiplication(Pol4, Ry);
+        multiplication(Pol5, Ry);
+        multiplication(Pol6, Ry);
+        multiplication(Pol1, Rz);
+        multiplication(Pol2, Rz);
+        multiplication(Pol3, Rz);
+        multiplication(Pol4, Rz);
+        multiplication(Pol5, Rz);
+        multiplication(Pol6, Rz);
+
+        draw_poly(Pol1);
+        draw_poly(Pol2);
+        draw_poly(Pol3);
+        draw_poly(Pol4);
+        draw_poly(Pol5);
+        draw_poly(Pol6);
         Sleep(50);
         Clear();
 
